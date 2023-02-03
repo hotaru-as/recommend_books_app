9a7aaa13cb8e0be5d5de6865473cbf4625702086
hotaru <vanilla.airi@gmail.com>
2023-02-03 13:01:26 +0900

    入力できる条件を追加

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -8,17 +8,34 @@ const SearchBooks = () => {
     axios.get(`https://app.rakuten.co.jp/services/api/BooksTotal/Search/20170404`, {
       params: {
         applicationId: '1097620706195684147',
-        keyword: 'typescript'
+        keyword: keyword
       }
     })
     .then(res => console.log(res.data))
   }
 
+  const changeKeyword = (value) => {
+    setKeyword(value);
+    console.log(keyword);
+  }
+
   return (
     <>
-      キーワード
-      <input type="text" onChange="{}"></input>
-      <button onClick={() => getBooks()}>本日のおすすめ本を見る</button>
+    <div>
+      <p>キーワード</p>
+      <input type="text" onChange={evt => changeKeyword(evt.target.value)}></input>
+    </div>
+    <div>
+      <p>ジャンル</p>
+      <input type="text" onChange={evt => changeKeyword(evt.target.value)}></input>
+    </div>
+    <div>
+      <p>金額</p>
+      <input type="text" onChange={evt => changeKeyword(evt.target.value)}></input>円
+      ～
+      <input type="text" onChange={evt => changeKeyword(evt.target.value)}></input>円
+    </div>
+    <button onClick={() => getBooks()}>本日のおすすめ本を見る</button>
     </>
   )
 }
